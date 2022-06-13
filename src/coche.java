627337b9fc944ef79b1c4fcebecd596cf55fe8cf
intofire <by.bryam@gmail.com>
2022-06-13 00:52:19 -0500

    Initial commit

---
+++ src/coche.java
@@ -0,0 +1,11 @@
+public class coche {
+    public static void main(String[] args) {
+        int npuertas = 1;
+        var miCoche = puertas(npuertas);
+        System.out.println(miCoche);
+    }
+
+    public static int puertas(int npuertas) {
+        return npuertas + 1;
+    }
+}
---
+++ src/suma.java
@@ -0,0 +1,14 @@
+public class suma {
+    public static void main(String[] args) {
+        //declarar variable
+        int resultado;
+        //llamar función
+        resultado = suma(15, 10, 20);
+        System.out.println(resultado);
+    }
+    //función suma
+    public static int suma(int a, int b, int c) {
+        return a + b +c;
+    }
+
+}
